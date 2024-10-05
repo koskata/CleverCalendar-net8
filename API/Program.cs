ad4f6670c4653f219668c44b088806d06b2365fc
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-10-05 20:11:08 +0300

    Added DbContext to the app

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,15 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<CleverCalendarContext>(opt => {
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
