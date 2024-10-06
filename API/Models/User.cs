86ab06c327b866c2af4df5f4ca12b21b561494d3
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-10-06 13:53:06 +0300

    Created class

---
+++ API/Models/User.cs
@@ -0,0 +1,18 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models;
+
+public class User
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    public string Name { get; set; } = string.Empty;
+
+    public byte[] PasswordHash { get; set; } = [];
+
+    public byte[] PasswordSalt { get; set; } = [];
+
+    public IEnumerable<Event> Events { get; set; } = [];
+}
