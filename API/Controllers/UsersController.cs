54ebedc392b538bedc552c50aaf01a08883508bc
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-10-12 15:08:29 +0300

    Implemented action for the testing controller

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,11 +1,22 @@
 using System;
+using API.Data;
+using API.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class UsersController : ControllerBase
+public class UsersController(CleverCalendarContext context) : ControllerBase
 {
     // Write logic 
+
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<User>>> GetUsers() 
+    {
+        var users = await context.Users.ToListAsync();
+
+        return users;
+    }
 }
