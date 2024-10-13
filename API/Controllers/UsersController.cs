e74e6e4fa4bda2ba74fd4e0fe380f6cb4ab8920e
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-10-13 15:15:13 +0300

    Implemented action for getting specific user by id

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,7 @@
 using System;
 using API.Data;
 using API.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,12 +10,27 @@ namespace API.Controllers;
 public class UsersController(CleverCalendarContext context) : BaseApiController
 {
     // Write logic 
-
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<IEnumerable<User>>> GetUsers() 
     {
         var users = await context.Users.ToListAsync();
 
         return users;
+    }
+
+    // Test action
+    [Authorize]
+    [HttpGet("{id:Guid}")]
+    public async Task<ActionResult<User>> GetUser(Guid id)
+    {
+        var user = await context.Users.FirstOrDefaultAsync(x => x.Id == id);
+
+        if (user == null)
+        {
+            return NotFound();
+        }
+
+        return user;
     }
 }
