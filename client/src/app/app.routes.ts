d662f855fe4f3a5ac0d1e766bbb613010e89ae53
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-10-29 16:34:45 +0200

    Added routes

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { EventListComponent } from './events/event-list/event-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: HomeComponent},
+    {path: 'events', component: EventListComponent},
+    {path: '**', component: HomeComponent, pathMatch: 'full'},
+];
