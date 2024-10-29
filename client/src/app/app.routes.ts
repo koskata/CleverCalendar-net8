3c63b03c03f784351800451fca0d9dcec779ed16
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-10-29 17:12:48 +0200

    Added auth guard to the home component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EventListComponent } from './events/event-list/event-list.component';
+import { AuthGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
+    {path: '', component: HomeComponent, canActivate: [AuthGuard]},
     {path: 'events', component: EventListComponent},
-    {path: '**', component: HomeComponent, pathMatch: 'full'},
+    {path: '**', redirectTo: ''},
 ];
