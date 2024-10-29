5fec7d26be6aa3592c27d15e3ab135fe1240f89f
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-10-29 23:39:58 +0200

    Added auth guard to the right component

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,10 +1,21 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { EventListComponent } from './events/event-list/event-list.component';
-import { AuthGuard } from './_guards/auth.guard';
+import { EventLikedComponent } from './events/event-liked/event-liked.component';
+import { EventScheduleComponent } from './events/event-schedule/event-schedule.component';
+import { authGuard } from './_guards/auth.guard';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent, canActivate: [AuthGuard]},
-    {path: 'events', component: EventListComponent},
-    {path: '**', redirectTo: ''},
+    { path: '', component: HomeComponent },
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [authGuard],
+        children: [
+            { path: 'events', component: EventListComponent, canActivate: [authGuard] },
+            { path: 'events/liked', component: EventLikedComponent },
+            { path: 'events/schedule', component: EventScheduleComponent },
+        ]
+    },
+    { path: '**', component: HomeComponent, pathMatch: 'full' },
 ];
