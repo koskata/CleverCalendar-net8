a151b750f8946a93b323409d65a650aeee86bdc9
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-10-29 23:40:46 +0200

    Created component

---
+++ client/src/app/events/event-liked/event-liked.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-event-liked',
+  standalone: true,
+  imports: [],
+  templateUrl: './event-liked.component.html',
+  styleUrl: './event-liked.component.css'
+})
+export class EventLikedComponent {
+
+}
