89f9f0f1881a5bbd73edd6fa4ad27fd986177657
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-11-02 18:24:28 +0200

    Created service

---
+++ client/src/app/_services/events.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { AccountService } from './account.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EventsService {
+  private http = inject(HttpClient);
+  private accountService = inject(AccountService);
+  baseUrl = environment.apiUrl;
+  
+  getEvents() {
+    return this.http.get<Event[]>(this.baseUrl + 'event', this.getHttpOptions());
+  }
+
+  getEvent(id: number) {
+    return this.http.get<Event>(this.baseUrl + 'event/' + id, this.getHttpOptions());
+  }
+
+  getHttpOptions() {
+    return {
+      headers: new HttpHeaders({
+        Authorization: `Bearer ${this.accountService.currentUser()?.token}`
+      })
+    }
+  }
+}
