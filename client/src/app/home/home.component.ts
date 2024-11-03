f999d24c876a278fcc402e59e5e48480b781eea5
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-11-03 18:26:34 +0200

    Removed unnecessary methods

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -9,15 +9,11 @@ import { HttpClient } from '@angular/common/http';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent{
   
   http = inject(HttpClient);
   registerMode = false;
   users: any;
-
-  ngOnInit(): void {
-    this.getUsers();
-  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
@@ -25,13 +21,5 @@ export class HomeComponent implements OnInit {
 
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: () => console.log('Request has completed')
-    })
   }
 }
