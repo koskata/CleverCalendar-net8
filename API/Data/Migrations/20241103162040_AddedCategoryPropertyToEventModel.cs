b2c627219fa267cfe51ef39a81973949d0848b54
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-11-03 18:23:37 +0200

    Added category property for every event

---
+++ API/Data/Migrations/20241103162040_AddedCategoryPropertyToEventModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddedCategoryPropertyToEventModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Category",
+                table: "Events",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Category",
+                table: "Events");
+        }
+    }
+}
