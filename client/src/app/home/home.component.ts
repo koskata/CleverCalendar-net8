c83eba56d28d2283152a8a14f8dcbb604dad5f59
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-11-09 18:05:08 +0200

    Made register mode to enable only when user is not logged in

--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,22 +1,32 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RegisterComponent } from "../register/register.component";
 import { HttpClient } from '@angular/common/http';
+import { AccountService } from '../_services/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RegisterComponent],
+  imports: [RegisterComponent, RouterLink],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent{
-  
+  private accountService = inject(AccountService);
+  toastr = inject(ToastrService);
   http = inject(HttpClient);
   registerMode = false;
   users: any;
 
   registerToggle() {
-    this.registerMode = !this.registerMode;
+    if (!this.accountService.currentUser()) {
+      this.registerMode = !this.registerMode;
+    }
+    else {
+      this.toastr.error("You already have an account!");
+    }
+    
   }
 
   cancelRegisterMode(event: boolean) {
