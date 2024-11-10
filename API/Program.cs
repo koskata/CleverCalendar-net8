21a2bfbb0dafb21a45d8a5aad3682f1a942bfcb4
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-11-10 13:42:13 +0200

    Migrating automatic when building project and seeded categories

--- API/Program.cs
+++ API/Program.cs
@@ -27,4 +27,19 @@ app.UseAuthorization();
 
 app.MapControllers();
 
+using var scope  = app.Services.CreateScope();
+
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<CleverCalendarContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedCategories(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
+
 app.Run();
