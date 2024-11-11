30a09e2bd2f90155d46e2c6d21b1cd44865be4b9
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-11-11 22:54:05 +0200

    Implemented methods for closing the modal and modalToggle, but not working

--- client/src/app/events/event-list/event-list.component.ts
+++ client/src/app/events/event-list/event-list.component.ts
@@ -1,14 +1,15 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { EventsService } from '../../_services/events.service';
 import { Event } from '../../_models/event';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { Day } from '../../_models/day';
 import { RouterLink } from '@angular/router';
+import { EventCreateModalComponent } from "../event-create-modal/event-create-modal.component";
 
 @Component({
   selector: 'app-event-list',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, EventCreateModalComponent, NgIf],
   templateUrl: './event-list.component.html',
   styleUrl: './event-list.component.css'
 })
@@ -19,6 +20,7 @@ export class EventListComponent implements OnInit {
   daysOfWeek: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   monthName: string = '';
   monthEmoticon: string = '';
+  showModal = false;
 
   ngOnInit(): void {
     this.setMonthEmoticons();
@@ -37,6 +39,15 @@ export class EventListComponent implements OnInit {
     const { monthName, monthEmoticon } = this.eventService.setMonthEmoticons();
     this.monthName = monthName;
     this.monthEmoticon = monthEmoticon;
+  }
+  
+  modalToggle() {
+    this.showModal = !this.showModal;
+  }
+
+  closeModal(event: boolean) {
+    console.log('Modal close event received');
+    this.showModal = event;
   }
 
 }
