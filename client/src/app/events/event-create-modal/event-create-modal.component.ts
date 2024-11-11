54274790a0b97c25da3b139e53f0df0b17c4f747
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-11-11 22:56:05 +0200

    Implemented methods for creating and closing modal

--- client/src/app/events/event-create-modal/event-create-modal.component.ts
+++ client/src/app/events/event-create-modal/event-create-modal.component.ts
@@ -1,12 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, inject, output, Output, signal } from '@angular/core';
+import { EventsService } from '../../_services/events.service';
+import { Event } from '../../_models/event';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-event-create-modal',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './event-create-modal.component.html',
   styleUrl: './event-create-modal.component.css'
 })
 export class EventCreateModalComponent {
+  private eventService = inject(EventsService);
+  closeModal = output<boolean>();
+  // event: Partial<Event> = {
+  //   name: '',
+  //   start: new Date(),
+  //   end: new Date(),
+  //   location: '',
+  //   categoryId: 1,
+  // };
 
+  model: any;
+
+  create() {
+    // if (this.event.name && this.event.start && this.event.end) {
+      console.log('Form submitted');
+      this.eventService.createEvent(this.model).subscribe({
+        next: response => {
+          console.log(response);
+          this.close();
+        },
+      });
+    // }
+  }
+
+  close() {
+    console.log('Close button clicked');
+    this.closeModal.emit(false);
+  }
 }
