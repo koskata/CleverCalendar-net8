60a1a4e77da14df7ac464b2098be5c308fb2c5a6
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-11-13 22:32:25 +0200

    Implemented method for creating event

--- API/Interfaces/Event/IEventService.cs
+++ API/Interfaces/Event/IEventService.cs
@@ -6,4 +6,6 @@ namespace API.Interfaces.Event;
 public interface IEventService
 {
     Task<List<EventCategoryDto>> GetAllEventCategoriesAsync();
+
+    Task<API.Models.Event> CreateEventAsync(EventDto eventDto, string userId);
 }
--- API/Services/EventService.cs
+++ API/Services/EventService.cs
@@ -3,6 +3,7 @@ using System.IO.Compression;
 using API.Data;
 using API.DTOs;
 using API.Interfaces.Event;
+using API.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Services;
@@ -10,6 +11,27 @@ namespace API.Services;
 public class EventService(CleverCalendarContext _context) : IEventService
 {
     private readonly CleverCalendarContext context = _context;
+
+    public async Task<Event> CreateEventAsync(EventDto eventDto, string userId)
+    {
+        var eventModel = new Event() {
+            Name = eventDto.Name,
+            Start = DateTime.Parse(eventDto.Start),
+            End = DateTime.Parse(eventDto.End),
+            UserId = Guid.Parse(userId),
+            CategoryId = eventDto.Category.Id
+        };
+
+        if (eventDto.Location is not null)
+        {
+            eventModel.Location = eventDto.Location;
+        }
+
+        await context.Events.AddAsync(eventModel);
+        await context.SaveChangesAsync();
+
+        return eventModel;
+    }
 
     public async Task<List<EventCategoryDto>> GetAllEventCategoriesAsync()
     {
