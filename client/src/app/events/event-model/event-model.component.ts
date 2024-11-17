ac75f4898dc7abc5f51eb5fd35eb8ce524729d63
Konstantin Ganev <66053722+koskata@users.noreply.github.com>
2024-11-17 22:08:05 +0200

    Routing to event details with event Id

--- client/src/app/events/event-model/event-model.component.ts
+++ client/src/app/events/event-model/event-model.component.ts
@@ -1,11 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { EventsService } from '../../_services/events.service';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-event-model',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './event-model.component.html',
   styleUrl: './event-model.component.css'
 })
